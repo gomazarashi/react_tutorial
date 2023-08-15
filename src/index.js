3d4ce8af3f460e4aa06293ded7319a7c8e4a7d59
gomazarashi <Seto42357@gmail.com>
2023-08-15 12:19:48 +0900

    [add]handleClickを定義

--- src/index.js
+++ src/index.js
@@ -23,10 +23,19 @@ class Board extends React.Component {
         };
     }
 
+    handleClick(i) {
+        const squares = this.state.squares.slice();
+        squares[i] = 'X';
+        this.setState({ squares: squares });
+    }
+
     renderSquare(i) {
-        return <Square 
-        value={this.state.squares[i]
-        onclick={()=>this.handleClick(i)}} />;
+        return (
+            <Square
+                value={this.state.squares[i]}
+                onclick={() => this.handleClick(i)}
+            />
+        );
     }
 
     render() {
